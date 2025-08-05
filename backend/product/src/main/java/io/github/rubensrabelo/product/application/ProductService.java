15f0b3cf98cfc7681a6ecaf7805c7821034d856e
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-08-05 09:47:54 -0300

    feat(product): complete product service implementation

--- backend/product/src/main/java/io/github/rubensrabelo/product/application/ProductService.java
+++ backend/product/src/main/java/io/github/rubensrabelo/product/application/ProductService.java
@@ -1,7 +1,58 @@
 package io.github.rubensrabelo.product.application;
 
+import io.github.rubensrabelo.product.application.dto.ProductCreateDTO;
+import io.github.rubensrabelo.product.application.dto.ProductResponseDTO;
+import io.github.rubensrabelo.product.application.dto.ProductUpdateDTO;
+import io.github.rubensrabelo.product.application.exceptions.ResourceNotFoundException;
+import io.github.rubensrabelo.product.domain.Product;
+import io.github.rubensrabelo.product.infra.repository.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
+
+    private final ProductRepository repository;
+    private final ModelMapper modelMapper;
+
+    public ProductService(ProductRepository repository, ModelMapper modelMapper) {
+        this.repository = repository;
+        this.modelMapper = modelMapper;
+    }
+
+    public Page<ProductResponseDTO> findAll(Pageable pageable) {
+        Page<Product> entities = repository.findAll(pageable);
+        return entities.map(
+                entity -> modelMapper.map(entity, ProductResponseDTO.class)
+        );
+    }
+
+    public ProductResponseDTO findById(Long id) {
+        Product entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found."));
+        return modelMapper.map(entity, ProductResponseDTO.class);
+    }
+
+    public ProductResponseDTO create(ProductCreateDTO dtoCreate) {
+        Product entity = modelMapper.map(dtoCreate, Product.class);
+        entity = repository.save(entity);
+        return modelMapper.map(entity, ProductResponseDTO.class);
+    }
+
+    public ProductResponseDTO update(Long id, ProductUpdateDTO dtoUpdate) {
+        Product entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found."));
+        updateData(dtoUpdate, entity);
+        return modelMapper.map(entity, ProductResponseDTO.class);
+    }
+
+    private void updateData(ProductUpdateDTO dtoUpdate, Product entity) {
+        entity.setName(dtoUpdate.getName());
+        entity.setPrice(dtoUpdate.getPrice());
+    }
 }
---
+++ backend/product/src/main/java/io/github/rubensrabelo/product/application/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package io.github.rubensrabelo.product.application.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
