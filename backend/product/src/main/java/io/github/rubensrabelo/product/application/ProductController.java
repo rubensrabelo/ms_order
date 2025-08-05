403e2b5ceb5b0b59208133ce755110231e565efb
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-08-05 10:42:40 -0300

    feat(product): finish implementing ProductController

--- backend/product/src/main/java/io/github/rubensrabelo/product/application/ProductController.java
+++ backend/product/src/main/java/io/github/rubensrabelo/product/application/ProductController.java
@@ -3,13 +3,14 @@ package io.github.rubensrabelo.product.application;
 import io.github.rubensrabelo.product.application.dto.ProductCreateDTO;
 import io.github.rubensrabelo.product.application.dto.ProductResponseDTO;
 import io.github.rubensrabelo.product.application.dto.ProductUpdateDTO;
-import io.github.rubensrabelo.product.application.exceptions.ResourceNotFoundException;
-import io.github.rubensrabelo.product.domain.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("products")
@@ -27,14 +28,19 @@ public class ProductController {
     }
 
     public ResponseEntity<ProductResponseDTO> findById(Long id) {
-        return null;
+        ProductResponseDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
     }
 
     public ResponseEntity<ProductResponseDTO> create(ProductCreateDTO dtoCreate) {
-        return null;
+        ProductResponseDTO dto = service.create(dtoCreate);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
     public ResponseEntity<ProductResponseDTO> update(Long id, ProductUpdateDTO dtoUpdate) {
-        return null;
+        ProductResponseDTO dto = service.update(id, dtoUpdate);
+        return ResponseEntity.ok().body(dto);
     }
 }
