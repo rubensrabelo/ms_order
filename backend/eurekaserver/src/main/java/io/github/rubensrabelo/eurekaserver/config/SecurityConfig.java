dde1aab7d9c07851128caa40837f8926ef5a7821
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-08-06 07:07:59 -0300

    feat(eureka-server): add Eureka Server with Spring Security configuration

---
+++ backend/eurekaserver/src/main/java/io/github/rubensrabelo/eurekaserver/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package io.github.rubensrabelo.eurekaserver.config;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableEurekaServer
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
