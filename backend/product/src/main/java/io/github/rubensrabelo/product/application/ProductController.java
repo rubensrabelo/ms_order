69bd3278b3f399c9bc2679ea05f3629f189c6f4a
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-08-06 06:53:19 -0300

    fix(controller): add missing annotations to ProductController

--- backend/product/src/main/java/io/github/rubensrabelo/product/application/ProductController.java
+++ backend/product/src/main/java/io/github/rubensrabelo/product/application/ProductController.java
@@ -4,10 +4,12 @@ import io.github.rubensrabelo.product.application.dto.ProductCreateDTO;
 import io.github.rubensrabelo.product.application.dto.ProductResponseDTO;
 import io.github.rubensrabelo.product.application.dto.ProductUpdateDTO;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -15,31 +17,49 @@ import java.net.URI;
 @RestController
 @RequestMapping("products")
 public class ProductController {
-
     private final ProductService service;
 
     public ProductController(ProductService service) {
         this.service = service;
     }
 
-    public ResponseEntity<Page<ProductResponseDTO>> findAll(Pageable pageable) {
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Page<ProductResponseDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "10") Integer size,
+            @RequestParam(value = "direction", defaultValue = "asc") String direction
+    ) {
+        var sortDirection = direction.equalsIgnoreCase("asc") ? Direction.ASC : Direction.DESC;
+        Pageable pageable = PageRequest.of(page, size, sortDirection);
         Page<ProductResponseDTO> dto = service.findAll(pageable);
         return ResponseEntity.ok().body(dto);
     }
 
-    public ResponseEntity<ProductResponseDTO> findById(Long id) {
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<ProductResponseDTO> findById(@PathVariable Long id) {
         ProductResponseDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
     }
 
-    public ResponseEntity<ProductResponseDTO> create(ProductCreateDTO dtoCreate) {
+    @PostMapping(
+            produces = {MediaType.APPLICATION_JSON_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public ResponseEntity<ProductResponseDTO> create(@RequestBody ProductCreateDTO dtoCreate) {
         ProductResponseDTO dto = service.create(dtoCreate);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
     }
 
-    public ResponseEntity<ProductResponseDTO> update(Long id, ProductUpdateDTO dtoUpdate) {
+    @PutMapping(
+            value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public ResponseEntity<ProductResponseDTO> update(
+            @PathVariable Long id,
+            @RequestBody ProductUpdateDTO dtoUpdate) {
         ProductResponseDTO dto = service.update(id, dtoUpdate);
         return ResponseEntity.ok().body(dto);
     }
