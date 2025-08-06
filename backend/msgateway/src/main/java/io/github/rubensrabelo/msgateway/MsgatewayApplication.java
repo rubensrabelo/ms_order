8a6667c425be109d321cd856cc8c02be85cf26d3
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-08-06 12:42:48 -0300

    feat(gateway): add API Gateway

---
+++ backend/msgateway/src/main/java/io/github/rubensrabelo/msgateway/MsgatewayApplication.java
@@ -0,0 +1,25 @@
+package io.github.rubensrabelo.msgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MsgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgatewayApplication.class, args);
+	}
+
+	@Bean
+	RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+				.route(r -> r.path("/products/**").uri("lb://msproduct"))
+				.build();
+	}
+}
