efc74e6d2e56a71966fea7ac13180a72c016867c
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-08-13 09:00:15 -0300

    wip(order): work on PostgreSQL connection setup for Order microservice

--- backend/order/src/main/resources/db/migration/V1__Create_Table_Order.sql
+++ backend/order/src/main/resources/db/migration/V1__Create_Table_Order.sql
@@ -1,4 +1,4 @@
-CREATE TABLE tb_order IF NOT EXISTS tb_order (
+CREATE TABLE IF NOT EXISTS tb_order (
                           id SERIAL PRIMARY KEY,
                           created TIMESTAMP NOT NULL,
                           total_amount DOUBLE PRECISION NOT NULL
